555b1a26d1ab0b96d409857c81a817914ea2eff5
Mohammad Mojtaba Roshani-TOMaaR <mohammadmojtabaroshani@outlook.com>
2022-05-23 22:28:05 +0430

    AddDocker|befor/TASK1/branch

--- Task-1/part-A_DataExtraction/API/config/db.js
+++ Task-1/part-A_DataExtraction/API/config/db.js
@@ -1,5 +1,6 @@
 //DOCKER
 module.exports = {
   connectionString:
-    "mongodb://root:rootpassword@localhost:27017/?authMechanism=DEFAULT",
+    // "mongodb://root:rootpassword@localhost:27017/?authMechanism=DEFAULT"
+    "mongodb://127.0.0.1/DK",
 };
