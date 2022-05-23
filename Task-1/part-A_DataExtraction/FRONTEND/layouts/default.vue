37023fb3dd74f87fd36541e04e29d245ebc6c4e4
Mohammad Mojtaba Roshani-TOMaaR <mohammadmojtabaroshani@outlook.com>
2022-05-24 01:14:33 +0430

    fix directions|fornt dosent work

---
+++ Task-1/part-A_DataExtraction/FRONTEND/layouts/default.vue
@@ -0,0 +1,133 @@
+<template>
+  <v-app dark>
+<!--    <v-navigation-drawer-->
+<!--      v-model="drawer"-->
+<!--      :mini-variant="miniVariant"-->
+<!--      :clipped="clipped"-->
+<!--      fixed-->
+<!--      app-->
+<!--    >-->
+<!--      <v-list>-->
+<!--        <v-list-item-->
+<!--          v-for="(item, i) in items"-->
+<!--          :key="i"-->
+<!--          :to="item.to"-->
+<!--          router-->
+<!--          exact-->
+<!--        >-->
+<!--          <v-list-item-action>-->
+<!--            <v-icon>{{ item.icon }}</v-icon>-->
+<!--          </v-list-item-action>-->
+<!--          <v-list-item-content>-->
+<!--            <v-list-item-title v-text="item.title" />-->
+<!--          </v-list-item-content>-->
+<!--        </v-list-item>-->
+<!--      </v-list>-->
+<!--    </v-navigation-drawer>-->
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="rightDrawer = !rightDrawer" />
+<!--      <v-btn-->
+<!--        icon-->
+<!--        @click.stop="miniVariant = !miniVariant"-->
+<!--      >-->
+<!--        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>-->
+<!--      </v-btn>-->
+<!--      <v-btn-->
+<!--        icon-->
+<!--        @click.stop="clipped = !clipped"-->
+<!--      >-->
+<!--        <v-icon>mdi-application</v-icon>-->
+<!--      </v-btn>-->
+<!--      <v-btn-->
+<!--        icon-->
+<!--        @click.stop="fixed = !fixed"-->
+<!--      >-->
+<!--        <v-icon>mdi-minus</v-icon>-->
+<!--      </v-btn>-->
+      <v-toolbar-title v-text="title" />
+<!--      <v-spacer />-->
+<!--      <v-btn-->
+<!--        icon-->
+<!--        @click.stop="rightDrawer = !rightDrawer"-->
+<!--      >-->
+<!--        <v-icon>mdi-menu</v-icon>-->
+<!--      </v-btn>-->
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click.native="right = !right" style="color: #a2c7ff;">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>RTL / LTR</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer
+      :absolute="!fixed"
+      app
+      class="justify-center"
+    >
+      <span>&copy;<a herf="https://github.com/MohammadMojtabaRoshani-TOMaaR">mmRoshani </a>{{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'خانه',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'دسته بندی اصلی',
+          to: '/category'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'DK Dataset'
+    }
+  }
+}
+</script>
---
+++ Task-1/part-A_DataExtraction/FRONTEND/pages/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+      </v-card>
+      <v-card>
+        <v-card-title class="headline justify-center">
+          <h3>
+            دیتاست دیجی کالا
+          </h3>
+        </v-card-title>
+        <v-card-text>
+          <hr class="my-3">
+          <p>این دیتا ست  به صورت زیر ارایه می گردد:<br></p>
+          <ul class="pr-4 pl-4">
+            <li>
+              <h5>
+                DK-Data
+              </h5>
+              <p class="justify-center">
+                اطلاعات ابتدا از دیجی کالا استخراج شده و سپس در دیتابیس ما ذخیره شده است. شما می توانید از طریق API ما به این اطلاعات دسترسی داشته باشید. دقت کنید که این اطلاعات با کمی تاخیر نسبت به تغییرات دیجی کالا بروز رسانی می گردند.
+              </p>
+            </li>
+          </ul>
+          <v-alert
+            border="right"
+            colored-border
+            type="error"
+            elevation="2"
+          >
+            <p>
+              پروژه فوق تنها برای استفاده های آموزش ایجاد و پشتیبانی میگردد و مسولیت هر گونه سواستفاده بر عهده شخص استفاده کننده میباشد.
+            </p>
+          </v-alert>
+          <v-alert
+            border="bottom"
+            colored-border
+            type="warning"
+            elevation="2"
+          >
+            <p>
+              قبل از استفاده از این پروژه می بایست شما به عنوان کابر مجاز تشخیص داده شوید و در صورت تشخیص استفاده غیر مجاز ip شما بلاک می گرددو
+            </p>
+            <p>
+              کاربران اصلی این سایت توسعه دهندگان ها هستند.
+            </p>
+          </v-alert>
+          <p>
+            برای اطلاعات بیشتر: <a
+              href="https://vuetifyjs.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              documentation
+            </a>.
+          </p>
+          <p>
+            چنل دیسکورد:  <a
+              href="https://chat.vuetifyjs.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="chat"
+            >
+              discord
+            </a>.
+          </p>
+          <p>
+            ارتباط با توسعه دهنده: <a
+              href="https://github.com/MohammadMojtabaRoshani-TOMaaR"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="contribute"
+            >
+              Github
+            </a>.
+          </p>
+          <hr class="my-3">
+
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            ادامه
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
