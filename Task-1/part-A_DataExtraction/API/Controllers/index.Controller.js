1e9c4c9cdcd1911890de78ba53115bb6fb75f5d2
Mohammad Mojtaba Roshani-TOMaaR <mohammadmojtabaroshani@outlook.com>
2022-05-25 16:54:20 +0430

    fetchProductsChart

--- Task-1/part-A_DataExtraction/API/Controllers/index.Controller.js
+++ Task-1/part-A_DataExtraction/API/Controllers/index.Controller.js
@@ -1,9 +1,38 @@
-var express = require("express");
-var router = express.Router();
-
+let express = require("express");
+let router = express.Router();
+let Product = require("../models/Products")
+const {json} = require("express");
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "SITE TOTAL ROUTES" });
+router.get("/", async function (req, res, next) {
+    let labels = []
+    let value =  []
+      let queryResult = await Product.aggregate([
+          {
+              $group: {
+                  _id: {
+                      $dateToString: {
+                          format: "%Y-%m-%d (%H:%M:%S o'clock)",
+                          date: "$modify_date"
+                      }
+                  },
+                  count: {
+                      $sum: 1
+                  }
+              }
+          },
+          {
+              $sort: {
+                  _id: 1
+              }
+          }
+      ])
+
+    await queryResult.forEach( element => {
+        labels.push(element._id)
+        value.push(element.count)
+    })
+
+  res.json({labels, value})
 });
 
 module.exports = router;
--- Task-1/part-A_DataExtraction/FRONTEND/pages/index.vue
+++ Task-1/part-A_DataExtraction/FRONTEND/pages/index.vue
@@ -20,31 +20,48 @@
               <p class="justify-center">
                 اطلاعات ابتدا از دیجی کالا استخراج شده و سپس در دیتابیس ما ذخیره شده است. شما می توانید از طریق API ما به این اطلاعات دسترسی داشته باشید. دقت کنید که این اطلاعات با کمی تاخیر نسبت به تغییرات دیجی کالا بروز رسانی می گردند.
               </p>
+              <p>
+                در زیر نمودار استخراج محصولات بر حسب تاریخ را مشاهده می کنید:
+              </p>
             </li>
           </ul>
-          <v-alert
-            border="right"
-            colored-border
-            type="error"
-            elevation="2"
-          >
-            <p>
-              پروژه فوق تنها برای استفاده های آموزش ایجاد و پشتیبانی میگردد و مسولیت هر گونه سواستفاده بر عهده شخص استفاده کننده میباشد.
-            </p>
-          </v-alert>
-          <v-alert
-            border="bottom"
-            colored-border
-            type="warning"
-            elevation="2"
-          >
-            <p>
-              قبل از استفاده از این پروژه می بایست شما به عنوان کابر مجاز تشخیص داده شوید و در صورت تشخیص استفاده غیر مجاز ip شما بلاک می گرددو
-            </p>
-            <p>
-              کاربران اصلی این سایت توسعه دهندگان ها هستند.
-            </p>
-          </v-alert>
+
+          <div class="ma-10">
+            <v-card
+              class="mt-4 mx-auto"
+              max-width="400"
+            >
+              <v-sheet
+                class="v-sheet--offset mx-auto"
+                color="cyan"
+                elevation="12"
+                max-width="calc(100% - 32px)"
+              >
+                <v-sparkline
+                  :labels="labels"
+                  :value="value"
+                  color="white"
+                  line-width="3"
+                  padding="16"
+                  stroke-linecap="round"
+                  smooth
+                ></v-sparkline>
+              </v-sheet>
+
+              <v-card-text class="pt-0">
+                <v-divider class="my-2"></v-divider>
+                <v-icon
+                  class="mr-2"
+                  small
+                >
+                  mdi-clock
+                </v-icon>
+                <span class="text-caption grey--text font-weight-light">last registration on {{labels[labels.length-1]}}</span>
+              </v-card-text>
+            </v-card>
+
+          </div>
+
           <p>
             برای اطلاعات بیشتر: <a
               href="https://vuetifyjs.com"
@@ -52,16 +69,6 @@
               rel="noopener noreferrer"
             >
               documentation
-            </a>.
-          </p>
-          <p>
-            چنل دیسکورد:  <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
             </a>.
           </p>
           <p>
@@ -94,6 +101,30 @@
 
 <script>
 export default {
-  name: 'IndexPage'
+  data: () => ({
+    name: 'IndexPage',
+    labels: [],
+    value: [],
+  }),
+  methods:{
+    async fetchSparklineData(){
+      await this.$axios.get("/")
+        .then( (response) => {
+          this.labels = response.data.labels
+          this.value = response.data.value
+        })
+        .catch(err => console.log(err))
+    }
+  },
+  created() {
+    this.fetchSparklineData()
+  }
 }
 </script>
+
+<style>
+.v-sheet--offset {
+  top: -24px;
+  position: relative;
+}
+</style>
